4049f05b008df26f0ef5be46adfbe3be438b2fac
Andrew Elliott <andrewcaelliott@gmail.com>
2016-03-01 16:57:42 +0000

    #Simple merge job submit form

--- dash/views.py
+++ dash/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
+from .forms import MergeForm
+from .models import MergeJob
 
 # Create your views here.
 
 
 def dash(request):
-    return render(request, 'dash/dash_base.html', {})
+    return render(request, 'dash/dash_home.html', {})
+
+def test(request):
+    mergeJob=MergeJob(
+    	template_folder="/Doc Merge/Templates",
+    	template="AddParty_v3.md",
+    	output_folder="/Doc Merge/Output",
+    	flow = "md",
+    	)
+    mergeForm = MergeForm(instance=mergeJob)
+    return render(request, 'dash/dash_test.html', {"mergeForm": mergeForm})
